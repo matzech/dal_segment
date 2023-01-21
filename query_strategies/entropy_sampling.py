ad021e01af654084535cb8419ae596592dd61741
Zech, Matthias <matthias.zech@dlr.de>
2023-01-21 13:24:37 +0100

    entropy for segmentation

--- query_strategies/entropy_sampling.py
+++ query_strategies/entropy_sampling.py
@@ -9,6 +9,11 @@ class EntropySampling(Strategy):
     def query(self, n):
         unlabeled_idxs, unlabeled_data = self.dataset.get_unlabeled_data()
         probs = self.predict_prob(unlabeled_data)
+        probs_1 = 1-probs 
         log_probs = torch.log(probs)
-        uncertainties = (probs*log_probs).sum(1)
+        log_probs_1 = torch.log(probs_1)
+        uncertainties = (probs*log_probs)
+        uncertainties_1 = (probs_1*log_probs_1)
+        uncertainties = uncertainties + uncertainties_1
+        uncertainties = uncertainties.sum([1,2,3])
         return unlabeled_idxs[uncertainties.sort()[1][:n]]
