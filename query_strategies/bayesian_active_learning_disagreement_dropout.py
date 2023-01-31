fbf85092b0393825f3fa3c752529b11c2950bad7
pieterhexen <hendrik-pieter.tetens@dlr.de>
2023-01-31 23:36:45 +0100

    update BALDDropout strat

--- query_strategies/bayesian_active_learning_disagreement_dropout.py
+++ query_strategies/bayesian_active_learning_disagreement_dropout.py
@@ -10,8 +10,4 @@ class BALDDropout(Strategy):
     def query(self, n):
         unlabeled_idxs, unlabeled_data = self.dataset.get_unlabeled_data()
         probs = self.predict_prob_dropout_split(unlabeled_data, n_drop=self.n_drop)
-        pb = probs.mean(0)
-        entropy1 = (-pb*torch.log(pb)).sum(1)
-        entropy2 = (-probs*torch.log(probs)).sum(2).mean(0)
-        uncertainties = entropy2 - entropy1
-        return unlabeled_idxs[uncertainties.sort()[1][:n]]
+        return unlabeled_idxs[probs.sort()[:n]]
